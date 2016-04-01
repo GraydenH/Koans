82a990c34180ddfe7323159e33330dc5c3ffecd5
grayden <raffledtickets@gmail.com>
2016-03-31 23:08:43 -0700

    finished collections

--- lesson3/task10/src/Task.kt
+++ lesson3/task10/src/Task.kt
@@ -1,4 +1,6 @@
 // Return the set of products that were ordered by every customer
-fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
-    TODO()
-}
+fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> =
+    customers.fold(customers.flatMap { orderedProducts }.toSet(), {
+        orderedByAll, customer -> customer.orders.flatMap { it.products }.toSet().intersect(orderedProducts)
+    })
+
--- lesson3/task12/src/Task.kt
+++ lesson3/task12/src/Task.kt
@@ -1,8 +1,7 @@
 fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
+    val groupsByLength = collection. groupBy { s -> s.length }
 
-    val groupsByLength = collection. groupBy { s -> TODO() }
-
-    val maximumSizeOfGroup = groupsByLength.values.map { group -> TODO() }.max()
-
-    return groupsByLength.values.firstOrNull { group -> TODO() }
+    val maximumSizeOfGroup = groupsByLength.values.map { group -> group.size }.max()
+    
+    return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
 }
